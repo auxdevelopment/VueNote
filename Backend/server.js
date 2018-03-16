ad284d2d4501a31b0a2d007c643a22d885995919
Benjamin Auinger <benjamin.au.99@gmx.at>
2018-03-16 17:46:16 +0100

    Initial commit

---
+++ Backend/server.js
@@ -0,0 +1,62 @@
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+
+const adapter = new FileSync('db.json');
+const db = low(adapter);
+
+db.defaults({
+    notes: []
+}).write();
+
+
+const express = require('express'),
+  app = express(),
+  port = process.env.PORT || 3000
+;
+
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+// Routes
+
+app.route('/notes').get(getAllNotes);
+app.route('/note').post(addNewNote);
+
+// End Routes
+
+function getAllNotes(req, res) {
+    const notes = db.get('notes');
+    res.json(notes);
+}
+
+/*
+A note looks like this:
+
+note: {
+    title: string;
+    content: string;
+}
+
+*/
+function addNewNote(req, res) {
+    const noteTitle = req.body.title;
+    const noteContent = req.body.content;
+
+    if (!noteTitle || !noteContent) {
+        res.status(500).json({ 'error': 'body must containt "title" and "content" attribute' });
+        return;
+    }
+
+    db.get('notes').push({
+        'title': noteTitle,
+        'content': noteContent
+    }).write();
+
+    res.json({ 'status': 'created' });
+}
+
+
+app.listen(port);
